206aacddb577f4092f28e0b7d667842006042804
Shinobi47 <HashiramaSenju47@gmail.com>
2020-03-25 00:49:28 +0100

    add random generated number endpoint and logging endpoint

--- src/main/java/com/benayed/app/controller/ApplicationController.java
+++ src/main/java/com/benayed/app/controller/ApplicationController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.benayed.app.dto.ProductDto;
@@ -36,4 +37,36 @@ public class ApplicationController {
 				: new ResponseEntity<>(notHiddenProducts, HttpStatus.OK);
 
 	}
+	
+	@GetMapping(value = "/log-message")
+	public String triggerLog(@RequestParam String logLevel) {
+		
+		if("INFO".equalsIgnoreCase(logLevel)) {
+			log.info("An info message as been logged");
+		}
+		else if ("DEBUG".equalsIgnoreCase(logLevel)) {
+			log.debug("A debug message has been logged");
+		}
+		else if ("WARN".equalsIgnoreCase(logLevel)) {
+			log.warn("A warn message has been logged");
+		}
+		
+		else if ("ERROR".equalsIgnoreCase(logLevel)) {
+			log.error("An error message has been logged");
+		}
+		
+		return "Triggered log with loglevel : " + logLevel;
+
+	}
+	
+	@GetMapping(value = "/log-random-number")
+	public String randomNumber() {
+		
+		Integer random = Double.valueOf(Math.random() * 5).intValue();
+		
+		log.info("Random number generated : " + random);
+		
+		return "radom number generated and logged : " + random;
+	}
+	
 }
